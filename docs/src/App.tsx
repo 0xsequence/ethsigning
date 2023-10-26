79ad97d9357a9db77c778de82bfd9188c2fd0785
Tolgahan Arikan <tolgahan.arikan@gmail.com>
2023-10-26 17:42:12 +0300

    Update router basename

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -3,16 +3,19 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Guide from './Guide'
 import Debugger from './Debugger'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Guide />
-  },
-  {
-    path: '/debugger',
-    element: <Debugger />
-  }
-])
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Guide />
+    },
+    {
+      path: '/debugger',
+      element: <Debugger />
+    }
+  ],
+  { basename: '/ethsigning/' }
+)
 
 function App() {
   return (
